1d21bec31f7a323bf0ee593466d13aaf45af84e8
zaryanz <aryanx404@gmail.com>
2024-02-05 11:37:43 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Aryan Singh on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
